71b743693eda387f7c16becdf28bc4853d3df958
Newmed20 <hamoda.style.000@gmail.com>
2024-06-27 12:14:44 +0100

    print a string

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: mjadid <mjadid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 16:01:05 by mjadid            #+#    #+#             */
-/*   Updated: 2024/06/21 16:18:59 by mjadid           ###   ########.fr       */
+/*   Updated: 2024/06/27 12:11:44 by mjadid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,59 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+int     ft_atoi(char *str)
+{
+    int result = 0;
+    int signe = 1;
+    int i =0;
 
+    while(((str[i] >= 9 & str[i] <= 13)||str[i] == 32) && str[i])
+        i++;
+    if(str[i] == '+' || str[i] == '-')
+    {
+        if(str[i] == '-')
+            signe  *= -1;
+        i++;
+    }
+    while(str[i])
+    {
+        result = result*10 + (str[i] - '0');
+        i++;
+    }
+    return(result*signe);
+}
 
+void    send_char(char c , int pid)
+{
+    int i = 7;
+    while(i>= 0)
+    {
+        if((c >> i)& 1)
+            kill(pid , SIGUSR1);
+        else
+            kill(pid , SIGUSR2);
+        usleep(200);
+        i--;   
+    }
+    
+}
 
 int main(int argc , char **argv)
 {
     int pid;
-    int signal_number;
+    int i = 0;
 
-    pid = atoi(argv[1]);
-    signal_number = atoi(argv[2]);
+    pid = ft_atoi(argv[1]);
 
-    while(1)
+    if(argc == 3 && pid > 0)
     {
-         if(signal_number == 1) {
-            kill(pid , SIGUSR1); // Send SIGUSR1 signal to the process
-        } else if(signal_number == 0) {
-            kill(pid , SIGUSR2); // Send SIGUSR2 signal to the process
+        while(argv[2][i])
+        {
+            send_char(argv[2][i] , pid);
+            i++;
         }
-        // sleep(1);
+        send_char('\0' , pid);
     }
+    
+    return 0;
 } 
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: mjadid <mjadid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 16:00:46 by mjadid            #+#    #+#             */
-/*   Updated: 2024/06/21 16:00:47 by mjadid           ###   ########.fr       */
+/*   Updated: 2024/06/27 12:13:10 by mjadid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,43 @@
 #include <signal.h>
 #include <unistd.h>
 
-void signal_handler(int signal) {
+void signal_handler(int signal , siginfo_t *info, void *ptr ) {
+
+    (void)ptr;
+    static char c = 0;
+    static int i = 7;
+    
     if(signal == SIGUSR1) {
-        printf("Received signal 1 (SIGUSR1)\n");
-    } else if(signal == SIGUSR2) {
-        printf("Received signal 0 (SIGUSR2)\n");
+        c |= 1 << i;
+    } 
+    i--;
+    if(i < 0) {
+        write(1, &c, 1);
+        c = 0;
+        i = 7;
     }
 }
+
 
 int main() {
+
+    struct sigaction sa;
+    
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = signal_handler;
+    
     printf("PID: %d\n", getpid());
-
-    // Register the signal handler for SIGUSR1 and SIGUSR2
-    signal(SIGUSR1, signal_handler);
-    signal(SIGUSR2, signal_handler);
-
-    while(1) {
+        
+    // if(sigaction(SIGUSR1 , &sa , NULL) == -1 || sigaction(SIGUSR2 , &sa , NULL) == -1)
+    // {
+    //     write(1 , "Error" ,6);
+    //     exit(1);
+    // }
+    sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+    while(1)
         pause(); // Wait for signals
-    }
-
+    
     return 0;
 }
+
