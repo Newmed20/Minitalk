424fa4c2032a48c3590e3009d8409524213ba3db
Newmed20 <hamoda.style.000@gmail.com>
2024-06-27 12:49:25 +0100

    fonction to write pid

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: mjadid <mjadid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 16:00:46 by mjadid            #+#    #+#             */
-/*   Updated: 2024/06/27 12:13:10 by mjadid           ###   ########.fr       */
+/*   Updated: 2024/06/27 12:48:28 by mjadid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,23 @@
 #include <signal.h>
 #include <unistd.h>
 
-void signal_handler(int signal , siginfo_t *info, void *ptr ) {
+void ft_putnbr(int nbr)
+{
+    char c;
+    
+    if(nbr >= 0 && nbr <= 9)
+    {
+        c = nbr + '0';
+        write(1 , &c , 1);
+    }
+    else {
+        ft_putnbr(nbr/10);
+        ft_putnbr(nbr%10);
+    }
+}
 
+void signal_handler(int signal , siginfo_t *info, void *ptr ) 
+{
     (void)ptr;
     static char c = 0;
     static int i = 7;
@@ -32,20 +47,20 @@ void signal_handler(int signal , siginfo_t *info, void *ptr ) {
 }
 
 
-int main() {
+int main() 
+{
 
     struct sigaction sa;
     
     sa.sa_flags = SA_SIGINFO;
     sa.sa_sigaction = signal_handler;
-    
-    printf("PID: %d\n", getpid());
-        
-    // if(sigaction(SIGUSR1 , &sa , NULL) == -1 || sigaction(SIGUSR2 , &sa , NULL) == -1)
-    // {
-    //     write(1 , "Error" ,6);
-    //     exit(1);
-    // }
+    write(1 , "PID OF SERVER IS : " , 20);
+    ft_putnbr(getpid()); 
+    if(sigaction(SIGUSR1 , &sa , NULL) == -1 || sigaction(SIGUSR2 , &sa , NULL) == -1)
+    {
+        write(1 , "Error" ,6);
+        exit(1);
+    }
     sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
     while(1)
@@ -53,4 +68,3 @@ int main() {
     
     return 0;
 }
-
